13901db538d238ed60224aedbf0f683386c34805
Ocean <1955840531@qq.com>
2025-02-27 17:07:19 +0800

    动态规划第一题

--- 新手动态规划合集/P1359 租用游艇.py
+++ 新手动态规划合集/P1359 租用游艇.py
@@ -1,1 +1,21 @@
-1
+def main():
+    n = int(input())
+    dp = [0] * (n+1)
+    temp = 0
+    information = []
+    for i in range(n - 1):
+        information.append(list(map(int, input().split())))
+    for a in range(len(information)):
+        i = a + 1
+        j = i
+        for b in range(len(information[a])):
+            j += 1
+            if dp[j] == 0:
+                dp[j] = information[a][b]
+            else:
+                dp[j] = min(dp[j], information[a][b] + dp[i])
+    print(dp[n])
+
+
+if __name__ == '__main__':
+    main()
