030af6ae32dd758755aa5bcc4c00961b48b981d6
Ocean <1955840531@qq.com>
2025-02-28 23:58:36 +0800

    2.28

--- 新手动态规划合集/P1060 [NOIP 2006 普及组] 开心的金明.py
+++ 新手动态规划合集/P1060 [NOIP 2006 普及组] 开心的金明.py
@@ -1,13 +1,13 @@
 def main():
-    total, n = map(int,input().split())
+    total, n = map(int, input().split())
     information = []
     dp = [0] * (total+1)
     for i in range(n):
-        information.append(list(map(int,input().split())))
+        information.append(list(map(int, input().split())))
     for i in range(n):
         temp = total
         while temp > information[i][0]:
-            dp[temp]=max(dp[temp],(information[i][1]*information[i][0])+dp[temp-information[i][0]])
+            dp[temp] = max(dp[temp], (information[i][1]*information[i][0])+dp[temp-information[i][0]])
             temp -= 1
     print(dp[total])
 
