829f41445f25b74aa3b0c4254009bf8c66e47b16
Ocean <1955840531@qq.com>
2025-03-04 14:20:09 +0800

    2025.3.4 区间背包问题，递归——记忆化搜索——动态规划dp

--- 新手动态规划合集/P2858 [USACO06FEB] Treats for the Cows.py
+++ 新手动态规划合集/P2858 [USACO06FEB] Treats for the Cows.py
@@ -1,25 +1,17 @@
-def solution(s, f, r, dp, day):
-
-
-
 def main():
     N = int(input())
     a = [0 for _ in range(N)]
-    dp = [0] * (N+1)
+    dp = [[0 for _ in range(N)] for _ in range(N)]
     for i in range(N):
-        a.append(int(input()))
-    front = 0
-    rear = N-1
-    solution(a, front, rear, dp, 1)
-    # for i in range(1, N+1):
-    #     dp[i] = min(dp[i-1]+a[front]*i, dp[i-1]+a[re
-    #     # print(dp[N])ar]*i)
-    #     if a[front] > a[rear]:
-    #         rear -= 1
-    #     else:
-    #         front += 1
+        a[i] = int(input())
+    for i in range(N):
+        dp[i][i] = a[i] * N
+    for i in range(N - 2, -1, -1):
+        for j in range(i + 1, N):
+            dp[i][j] = max(dp[i + 1][j] + a[i] * (N - j + i), dp[i][j - 1] + a[j] * (N - j + i))
+    print(dp[0][N - 1])
 
 
 if __name__ == '__main__':
     main()
-
+# 递归---记忆化搜索---动态规划
